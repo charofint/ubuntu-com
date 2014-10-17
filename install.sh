bd4db47bc2f943c1594f47e701b0ec1350221efb
shellbye <444854713@qq.com>
2014-10-17 09:56:44 +0800

    init

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# prepare
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+# some software
+sudo apt-get install -y vim
+sudo apt-get install -y git
+sudo apt-get install -y byobu
+sudo apt-get install -y python-pip
+# install the movie player VLC
+sudo apt-get install -y vlc browser-plugin-vlc
